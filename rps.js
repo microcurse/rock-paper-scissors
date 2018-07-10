2d69cba160c20bef9c71b05427dff65f815b0498
Marc <marc.maninang@gmail.com>
2018-07-10 13:33:30 -0700

    Working through game conditions and properly declaring a winner

--- rps.js
+++ rps.js
@@ -1,7 +1,6 @@
 function computerPlay () {
     var randNum = Math.floor(Math.random() * 10);
     var play = '';
-
     if(randNum <= 3){
         play = 'rock';
     } else if (randNum <= 6 && randNum > 3) {
@@ -9,37 +8,48 @@ function computerPlay () {
     } else {
         play = 'scissors';
     }
-
     return play;
 }
 
 function playRound(playerSelection, computerSelection) {
-
     if (playerSelection == 'rock' && computerSelection == 'scissors' ||
         playerSelection == 'paper' && computerSelection == 'rock' ||
         playerSelection == 'scissors' && computerSelection == 'paper') {
-            return 'You win!';
-            //return winMessage;
+            victory = true;
         } else if (playerSelection == 'paper' && computerSelection == 'scissors' ||
         playerSelection == 'scissors' && computerSelection == 'rock' ||
         playerSelection == 'rock' && computerSelection == 'paper') {
-            return 'You lose!';
-            //return loseMessage;
+            defeat = true;
         } else {
-            return "It's a tie..";
+            tie = true;
         }
 }
 
 function game() {
     /** This is where we keep score of the game. First to 5 wins */
+    let rounds = 0;
+    while (rounds <= 5) {
+        playRound();
+        if (victory = true) {
+            playerWins++
+            console.log("You win!");
+        } else if (defeat = true){
+            computerWins++
+            console.log("Computer wins!");
+        } else {
+            console.log("It's a tie!");
+        }
+        rounds++;
+    }
 }
-
 console.log('Rock..\nPaper..\nScissors..\nSHOOT!');
-
+var playerWins = 0;
+var computerWins = 0;
+var victory = false;
+var defeat = false;
+var tie = false;
 const computerSelection = computerPlay();
 const playerSelection = prompt('Type one: rock, paper, scissors').toLowerCase();
-
 console.log('Player picks: ' + playerSelection );
 console.log('Computer picks: ' + computerSelection );
-
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
