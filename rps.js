a0937ceb6d358d797e6bc87768b038d6e69074ea
Marc <marc.maninang@gmail.com>
2018-07-11 23:07:31 -0700

    Rewrote all functions. New goal is to simplify things to numbers

--- rps.js
+++ rps.js
@@ -1,37 +1,5 @@
-function computerPlay () {
-    var randNum = Math.floor(Math.random() * 10);
-    var play = '';
-    if(randNum <= 3){
-        play = 'rock';
-    } else if (randNum <= 6 && randNum > 3) {
-        play = 'paper';
-    } else {
-        play = 'scissors';
-    }
-    return play;
+function computerPlay(){
+    console.log(Math.floor((Math.random() * 3 ) + 1));
 }
-
-function playRound(playerSelection, computerSelection) {
-
-}
-
-function game() {
-    /** This is where we keep score of the game. First to 5 wins */
-    let rounds = 0;
-    while (rounds < 5) {
-        var winner = playRound();
-        rounds++;
-        console.log('Test ' + rounds + " " + winner);
-    }
-}
-console.log('Rock..\nPaper..\nScissors..\nSHOOT!');
-var playerWins = 0;
-var computerWins = 0;
-var victory = false;
-var defeat = false;
-var tie = false;
-const computerSelection = computerPlay();
-const playerSelection = prompt('Type one: rock, paper, scissors').toLowerCase();
-console.log('Player picks: ' + playerSelection );
-console.log('Computer picks: ' + computerSelection );
-console.log(game());
+computerPlay();
+// rock = 1, scissors = 2, paper = 3
