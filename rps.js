e61b42b1c966d7c5a0416f0049ea09aab260ce52
Marc <marc.maninang@gmail.com>
2018-07-22 19:29:03 -0700

    Added random computer rock paper scissor choices

--- rps.js
+++ rps.js
@@ -1,5 +1,16 @@
 function computerPlay(){
-    return (Math.floor((Math.random() * 3 ) + 1));
+    var choice = (Math.floor((Math.random() * 3 ) + 1));
+    if (choice == 1) {
+        choice = 'rock';
+    } else if (choice == 2) {
+        choice = 'paper';
+    } else {
+        choice = 'scissors';
+    }
+    return choice;
 }
-computerPlay();
+function player(){
+
+}
+console.log(computerPlay());
 // rock = 1, scissors = 2, paper = 3
